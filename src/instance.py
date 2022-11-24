f5df600d0caad0b397c2793b2636a088f33a6f34
Nim-ah <111262287+Nim-ah@users.noreply.github.com>
2022-11-23 23:22:50 -0500

    Added the data_extraction stuff to get_dataset fun

--- src/instance.py
+++ src/instance.py
@@ -1,6 +1,128 @@
 from typing import List
 
+import pandas as pd
 import numpy as np
+import random
+
+
+def get_dataset(num_students):
+    file = "Exam Sched Prog Datasets.xlsx"
+    #file = r"C:\Users\William Hazen\Documents\GitHub\exam-timetabling-smac\Exam Sched Prog Datasets.xlsx"
+    rooms = pd.read_excel(file, sheet_name = "datasets room caps ")
+    courses = pd.read_excel(file, sheet_name = "20221 course size")
+    enrolments = pd.read_excel(file, sheet_name = "20221 anonymized enrolments")
+    schedule = pd.read_excel(file, sheet_name = "20221 Final Schedule_fromLSM")
+    OG_Course_list = schedule.Course.dropna()
+    OG_Course_list = OG_Course_list.values
+    
+    # Data Extraction
+    #### Random sample of ID's
+    qwe = enrolments["HASHED_PERSON_ID"].values
+    qwer = np.unique(qwe)
+    #random_sample_names_generator = (qwer[i] for i in range(np.random.randint(0, len(qwer)-1)))
+    random_sample_names_generator = (qwer[i] for i in range(num_students))
+    name = []
+    
+    for i in random_sample_names_generator:
+        name.append(i)
+
+    Student_ID_List = random.sample(name, len(name))
+    print(len(Student_ID_List))
+    
+    #### Unique Exams for given student
+    Student_ID_Exams = []
+    #enrolments["HASHED_PERSON_ID"].loc[ID]
+    for ID in Student_ID_List:
+        Student_ID_Exams.append(enrolments["ACAD_ACT_CD"][enrolments.eq(ID).any(1)].values)
+    
+    exam_list = []
+    for exam in Student_ID_Exams:
+        exam_list.append(exam.any())
+    uniq_exams = np.unique(exam_list)
+    
+    new_enrol = pd.DataFrame(enrolments).copy()
+    new_enrol["class"] = 1
+    temp_df = new_enrol[new_enrol["HASHED_PERSON_ID"].isin(Student_ID_List)]
+    final_temp = temp_df[temp_df["ACAD_ACT_CD"].isin(uniq_exams)]
+    
+    #### Course Enrollment
+    
+    courses_enrollments_set = pd.pivot_table(data=final_temp, values="class", index="ACAD_ACT_CD", columns=["HASHED_PERSON_ID"], fill_value=0)
+    # Cleaning Room Table
+    #Cleaning room table
+
+    rooms['Room'] = rooms['Room'].astype(str)
+    rooms['Room'] = rooms['Bd'].str.cat(rooms['Room'], sep = " ")
+
+    rooms = rooms.drop(['Note','Bd'], axis=1)
+    # Cleaning Course Data
+    #Cleaning course data
+
+    courses['COURSE_CODE'] = courses['COURSE_CODE'].str.cat(courses['SECTIONCD'], sep = "")
+    courses = courses.drop(['SESSION_CD','SECTIONCD','ADMINFACULTYCODE','ADMINDEPT'], axis=1)
+    crselist = courses['COURSE_CODE'].tolist()
+    sizelist = courses['CURR_REG_QTY'].tolist()
+    # Enrolment data
+    idlist = enrolments['HASHED_PERSON_ID'].tolist()
+    classlist = enrolments['ACAD_ACT_CD'].tolist()
+
+    schedule['Room'] = schedule['Room'].astype(str)
+    schedule['Examdate'] = schedule['Examdate'].astype(str)
+    schedule['Begin'] = schedule['Begin'].astype(str)
+    schedule['Ends'] = schedule['Ends'].astype(str)
+
+    schedule['Room'] = schedule['Bd'].str.cat(schedule['Room'], sep = " ")
+
+    schedule = schedule.drop(['Bd','Faculty','Department','Section','Sesson','Course Name','Duration'], axis=1)
+
+    schedule.replace('ZZ REFER_TO_FACULTY_SCHEDULE', 'None', inplace=True)
+    schedule.replace('ZZ REFER_TO_FACULTY_SCHEDULE', 'None', inplace=True)
+    schedule.replace('ZZ ONLIN', 'None', inplace=True)
+    schedule.replace('APSCDept APSCDept', 'None', inplace=True)
+    schedule.replace('APSCDept ComputerLab', 'None', inplace=True)
+    schedule.replace('ZZ KNOX', 'None', inplace=True)
+    schedule.replace('ZZ VLAD', 'None', inplace=True)
+    schedule.replace('ZZ ONLINMUSIC', 'None', inplace=True)
+
+    OG_CL = list(OG_Course_list)
+
+    unique_course_list=[]
+    for course in OG_CL:
+        for crs in uniq_exams:
+            if course[0:8] == crs:
+                unique_course_list.append(course) 
+
+    new_schedule = pd.DataFrame(schedule).copy()
+    temp_sch_df = new_schedule[new_schedule["Course"].isin(unique_course_list)]
+    #Schedule data in list format
+
+    schdcrselist = temp_sch_df['Course'].tolist()
+    schdroomlist = temp_sch_df['Room'].tolist()
+    exmdatelist = temp_sch_df['Examdate'].tolist()
+    starttimelist = temp_sch_df['Begin'].tolist()
+    endtimelist = temp_sch_df['Ends'].tolist()
+    enrllist = temp_sch_df['Enrolment'].tolist()
+    crseinroomlist = temp_sch_df['Course In Room'].tolist()
+    roomcaplist = temp_sch_df['Room Cap'].tolist()
+    totalocclist = temp_sch_df['Total Occupancy'].tolist()
+    examdate_time_list = (temp_sch_df['Examdate'].astype(str) + " : " + temp_sch_df['Begin']).tolist()
+    course_enrollment_values = courses_enrollments_set.drop(["TEP445H1"], axis=0).values
+    sumHe_s = np.sum(course_enrollment_values, axis=1)
+
+    
+    E = schdcrselist
+    S = Student_ID_List
+    T = examdate_time_list
+    R = crseinroomlist
+    He_s = course_enrollment_values
+    sumHe_s = np.sum(He_s, axis=1)
+    Cp = roomcaplist
+
+
+
+
+
+
 
 
 def get_ET_instance(instance_num : int):
