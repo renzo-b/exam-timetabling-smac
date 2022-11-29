ec93b48a033d406b268827d0747f70fa44ffb3bb
WillHazenUofT <william.hazen@mail.utoronto.ca>
2022-11-29 16:14:01 -0500

    Redid data processing. Can select unique semester

--- src/instance.py
+++ src/instance.py
@@ -5,132 +5,139 @@ import numpy as np
 import pandas as pd
 
 ### Global instance space
+#Possible Semester dates: a_20189, a_20191, a_20195, a_20199, a_20201, a_20205, a_20215, a_20219, a_20221, a_20225, a_20229, a_20231
+
+
 INSTANCE_SPACE = [
-    {"num_students":np.random.randint(3000, 5500), 
-    "every_n_room":10, "np_seed": i, 
+    {"num_students":np.random.randint(50, 100), 
+    "every_n_room":10, 
+    "np_seed": i, 
     "room_avail_p": np.random.randint(95,100)/100, 
-    "prof_avail_p": np.random.randint(95,100)/100} for i in range(50)
+    "prof_avail_p": np.random.randint(95,100)/100,
+    "semester_date": "a_20189"} for i in range(10)
 ]
 
-def get_dataset(num_students, every_n_room, np_seed, room_avail_p, prof_avail_p):
+def get_dataset(num_students, every_n_room, np_seed, room_avail_p, prof_avail_p, semester_date):
     file = "Exam Sched Prog Datasets.xlsx"
     #file = r"C:\Users\William Hazen\Documents\GitHub\exam-timetabling-smac\Exam Sched Prog Datasets.xlsx"
     rooms = pd.read_excel(file, sheet_name = "datasets room caps ")
-    courses = pd.read_excel(file, sheet_name = "20221 course size")
-    enrolments = pd.read_excel(file, sheet_name = "20221 anonymized enrolments")
-    schedule = pd.read_excel(file, sheet_name = "20221 Final Schedule_fromLSM")
-    his_enrolments = pd.read_excel(file, sheet_name = "hist anonymized enrolments")
-    OG_Course_list = schedule.Course.dropna()
-    OG_Course_list = OG_Course_list.values
+    #courses_20221 = pd.read_excel(file, sheet_name = "20221 course size")
+    #enrolments_20221 = pd.read_excel(file, sheet_name = "20221 anonymized enrolments")
+    schedule_20221 = pd.read_excel(file, sheet_name = "20221 Final Schedule_fromLSM")
     
-    # Data Extraction
-    #### Random sample of ID's
-    qwe = his_enrolments["HASHED_PERSON_ID"].values
-    qwer = np.unique(qwe)
-    #random_sample_names_generator = (qwer[i] for i in range(np.random.randint(0, len(qwer)-1)))
-    random_sample_names_generator = (qwer[i] for i in range(num_students))
-    name = []
+    hist_anonymized_enrolments = pd.read_excel(file, sheet_name="hist anonymized enrolments")
+    #hist_course_size = pd.read_excel(file, sheet_name="hist course size ")
     
-    for i in random_sample_names_generator:
-        name.append(i)
-
-    Student_ID_List = random.sample(name, len(name))
-    
-    #### Unique Exams for given student
-    Student_ID_Exams = []
-    for ID in Student_ID_List:
-        Student_ID_Exams.append(his_enrolments["ACAD_ACT_CD"][his_enrolments.eq(ID).any(1)].values)
-    
-    exam_list = []
-    for exam in Student_ID_Exams:
-        exam_list.append(exam.any())
-    uniq_exams = np.unique(exam_list)
-    
-    new_enrol = pd.DataFrame(his_enrolments).copy()
-    new_enrol["class"] = 1
-    temp_df = new_enrol[new_enrol["HASHED_PERSON_ID"].isin(Student_ID_List)]
-    final_temp = temp_df[temp_df["ACAD_ACT_CD"].isin(uniq_exams)]
-    
-    #### Course Enrollment
-    
-    courses_enrollments_set = pd.pivot_table(data=final_temp, values="class", index="ACAD_ACT_CD", columns=["HASHED_PERSON_ID"], fill_value=0)
-    # Cleaning Room Table
-    #Cleaning room table 
+    #initalizing
+    #OG_Course_list = schedule_20221.Course.dropna().values
 
     rooms['Room'] = rooms['Room'].astype(str)
     rooms['Room'] = rooms['Bd'].str.cat(rooms['Room'], sep = " ")
 
-    rooms = rooms.drop(['Note','Bd'], axis=1)
-    # Cleaning Course Data
-    #Cleaning course data
+    #rooms = rooms.drop(['Note','Bd'], axis=1)
+    
+    # courses_20221['COURSE_CODE'] = courses_20221['COURSE_CODE'].str.cat(courses_20221['SECTIONCD'], sep = "")
+    # courses_20221 = courses_20221.drop(['SESSION_CD','SECTIONCD','ADMINFACULTYCODE','ADMINDEPT'], axis=1)
+    # crselist = courses_20221['COURSE_CODE'].tolist()
+    # sizelist = courses_20221['CURR_REG_QTY'].tolist()
+    # # Enrolment data
+    # idlist = enrolments_20221['HASHED_PERSON_ID'].tolist()
+    # classlist = enrolments_20221['ACAD_ACT_CD'].tolist()
 
-    courses['COURSE_CODE'] = courses['COURSE_CODE'].str.cat(courses['SECTIONCD'], sep = "")
-    courses = courses.drop(['SESSION_CD','SECTIONCD','ADMINFACULTYCODE','ADMINDEPT'], axis=1)
-    crselist = courses['COURSE_CODE'].tolist()
-    sizelist = courses['CURR_REG_QTY'].tolist()
-    # Enrolment data
-    idlist = his_enrolments['HASHED_PERSON_ID'].tolist()
-    classlist = his_enrolments['ACAD_ACT_CD'].tolist()
+    # schedule_20221['Room'] = schedule_20221['Room'].astype(str)
+    # schedule_20221['Examdate'] = schedule_20221['Examdate'].astype(str)
+    # schedule_20221['Begin'] = schedule_20221['Begin'].astype(str)
+    # schedule_20221['Ends'] = schedule_20221['Ends'].astype(str)
 
-    schedule['Room'] = schedule['Room'].astype(str)
-    schedule['Examdate'] = schedule['Examdate'].astype(str)
-    schedule['Begin'] = schedule['Begin'].astype(str)
-    schedule['Ends'] = schedule['Ends'].astype(str)
+    # schedule_20221['Room'] = schedule_20221['Bd'].str.cat(schedule_20221['Room'], sep = " ")
 
-    schedule['Room'] = schedule['Bd'].str.cat(schedule['Room'], sep = " ")
+    # schedule_20221 = schedule_20221.drop(['Bd','Faculty','Department','Section','Sesson','Course Name','Duration'], axis=1)
 
-    schedule = schedule.drop(['Bd','Faculty','Department','Section','Sesson','Course Name','Duration'], axis=1)
+    # schedule_20221.replace('ZZ REFER_TO_FACULTY_SCHEDULE', 'None', inplace=True)
+    # schedule_20221.replace('ZZ REFER_TO_FACULTY_SCHEDULE', 'None', inplace=True)
+    # schedule_20221.replace('ZZ ONLIN', 'None', inplace=True)
+    # schedule_20221.replace('APSCDept APSCDept', 'None', inplace=True)
+    # schedule_20221.replace('APSCDept ComputerLab', 'None', inplace=True)
+    # schedule_20221.replace('ZZ KNOX', 'None', inplace=True)
+    # schedule_20221.replace('ZZ VLAD', 'None', inplace=True)
+    # schedule_20221.replace('ZZ ONLINMUSIC', 'None', inplace=True)
 
-    schedule.replace('ZZ REFER_TO_FACULTY_SCHEDULE', 'None', inplace=True)
-    schedule.replace('ZZ REFER_TO_FACULTY_SCHEDULE', 'None', inplace=True)
-    schedule.replace('ZZ ONLIN', 'None', inplace=True)
-    schedule.replace('APSCDept APSCDept', 'None', inplace=True)
-    schedule.replace('APSCDept ComputerLab', 'None', inplace=True)
-    schedule.replace('ZZ KNOX', 'None', inplace=True)
-    schedule.replace('ZZ VLAD', 'None', inplace=True)
-    schedule.replace('ZZ ONLINMUSIC', 'None', inplace=True)
-
-    OG_CL = list(OG_Course_list)
-
-    unique_course_list=[]
-    for course in OG_CL:
-        for crs in uniq_exams:
-            if course[0:8] == crs:
-                unique_course_list.append(course) 
-
-    new_schedule = pd.DataFrame(schedule).copy()
-    temp_sch_df = new_schedule[new_schedule["Course"].isin(unique_course_list)]
-    #Schedule data in list format
-
-    schdcrselist = temp_sch_df['Course'].tolist()
-    schdroomlist = temp_sch_df['Room'].tolist()
-    exmdatelist = temp_sch_df['Examdate'].tolist()
-    starttimelist = temp_sch_df['Begin'].tolist()
-    endtimelist = temp_sch_df['Ends'].tolist()
-    enrllist = temp_sch_df['Enrolment'].tolist()
-    #crseinroomlist = temp_sch_df['Course In Room'].tolist()
-    #roomcaplist = temp_sch_df['Room Cap'].tolist()
-    totalocclist = temp_sch_df['Total Occupancy'].tolist()
-    examdate_time_list = (schedule['Examdate'].dropna().astype(str) + " : " + schedule['Begin']).dropna().tolist()
+    schedule_20221['Examdate'] = schedule_20221['Examdate'].astype(str)
+    schedule_20221['Begin'] = schedule_20221['Begin'].astype(str)
+    examdate_time_list = (schedule_20221['Examdate'].dropna().astype(str) + " : " + schedule_20221['Begin']).dropna().tolist()
+    
     ETL = np.unique(examdate_time_list)
     examdate_time_list = [i for i in ETL if i != "NaT : nan"]
-    course_enrollment_values = courses_enrollments_set.values #.drop(["TEP445H1"], axis=0).values
-    sumHe_s = np.sum(courses_enrollments_set, axis=1)
+    
+    # course_enrollment_values = courses_enrollments_set.values #.drop(["TEP445H1"], axis=0).values
+    # sumHe_s = np.sum(courses_enrollments_set, axis=1)
 
-    room_list = schedule['bd_room'].dropna().values
-    test_list = room_list.tolist()
-    roomlist = [i for i in test_list if i != "None"]
+    # room_list = schedule_20221['bd_room'].dropna().values
+    # test_list = room_list.tolist()
+    # roomlist = [i for i in test_list if i != "None"]
     
-    room_cap_list = schedule['Room Cap'].dropna().values
-    test_cap_list = room_cap_list.tolist()
-    roomcaplist = [i for i in test_cap_list if i != "None"]
+    # room_cap_list = schedule_20221['Room Cap'].dropna().values
+    # test_cap_list = room_cap_list.tolist()
+    # roomcaplist = [i for i in test_cap_list if i != "None"]
     
-    exam_set = uniq_exams
-    student_set = Student_ID_List
+    
+    #new processing
+    rooms['Room'] = rooms['Room'].astype(str)
+    rooms['Room'] = rooms['Bd'].str.cat(rooms['Room'], sep = " ")
+
+    ava_rooms = rooms['Room'].values
+    ava_room_cap = rooms['Room Cap'].values
+    hist_anonymized_enrolments['ACAD_ACT_CD'] = hist_anonymized_enrolments['ACAD_ACT_CD'].astype(str)
+    hist_anonymized_enrolments['ACAD_ACT_CD'] = hist_anonymized_enrolments['ACAD_ACT_CD'].str.cat(hist_anonymized_enrolments['SECTION_CD'])
+
+    semeser_list = np.unique(hist_anonymized_enrolments['SESSION_CD']).tolist()
+    anan_sem = {}
+    for sem in semeser_list:
+        anan_sem["a_{}".format(sem)] = hist_anonymized_enrolments[hist_anonymized_enrolments['SESSION_CD'] == sem]
+    
+    
+    def processing_fun(semester, num_student):
+        name = []
+        exam_list = []
+        Student_ID_Exams = []
+        
+        qwe = semester["HASHED_PERSON_ID"].values
+        qwer = np.unique(qwe)
+        random_sample_names_generator = (qwer[i] for i in range(num_student))
+    
+        for i in random_sample_names_generator:
+            name.append(i)
+            
+        Student_ID_List = random.sample(name, len(name))
+
+        for ID in Student_ID_List:
+            Student_ID_Exams.append(semester["ACAD_ACT_CD"][semester.eq(ID).any(1)].values)
+            
+        for exam in Student_ID_Exams:
+            exam_list.append(exam.any())
+            
+        uniq_exams = np.unique(exam_list)
+        new_enrol = pd.DataFrame(semester).copy()
+        new_enrol["class"] = 1
+
+        temp_df = new_enrol[new_enrol["HASHED_PERSON_ID"].isin(Student_ID_List)]
+        final_temp = temp_df[temp_df["ACAD_ACT_CD"].isin(uniq_exams)]
+
+        courses_enrollments_set = pd.pivot_table(data=final_temp, values="class", index="ACAD_ACT_CD", columns=["HASHED_PERSON_ID"], fill_value=0)
+        
+        return uniq_exams, Student_ID_List, courses_enrollments_set
+    
+    t_E, t_S, t_C = processing_fun(anan_sem[semester_date], num_students)
+    
+    #sumHe_s = np.sum(t_C.values, axis=1)
+    
+#storing    
+    exam_set = t_E
+    student_set = t_S
     datetime_slot_set = examdate_time_list
-    room_set = roomlist
-    room_capacity_set = roomcaplist
-    courses_enrollments_set = course_enrollment_values
+    room_set = ava_rooms
+    room_capacity_set = ava_room_cap
+    courses_enrollments_set = t_C.values
 
     # extract every N room to reduce the number of variables and constraints
     room_set = room_set[::every_n_room]
@@ -150,7 +157,7 @@ def get_dataset(num_students, every_n_room, np_seed, room_avail_p, prof_avail_p)
 def get_ET_instance(instance_num : int):   
 
     instance_config = INSTANCE_SPACE[instance_num]
-
+    
     # load dataset
     (exam_set, student_set, datetime_slot_set, room_set, 
     room_capacity_set, courses_enrollments_set, 
