4035b7b7878fe07c74b6572facd426c4eaa6f185
Renzo Benavides <renzo.benavides@mail.utoronto.ca>
2022-12-01 22:45:38 -0500

    Format code

--- src/instance.py
+++ src/instance.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 # Global instance space
 # Possible Semester dates: a_20189, a_20191, a_20195, a_20199, a_20201, a_20205, a_20215, a_20219, a_20221, a_20225, a_20229, a_20231
-''' 
+""" 
 {'a_len_20189': 5262,
  'a_len_20191': 5189,
  'a_len_20195': 496,
@@ -19,74 +19,90 @@ import pandas as pd
  'a_len_20225': 1111,
  'a_len_20229': 5916,
  'a_len_20231': 5861}
-'''
+"""
 
 student_per_semester = {
-    "a_20189" : [4000, 5262],
-    "a_20191" : [4000, 5189],
-    "a_20195" : [300, 496],
-    "a_20199" : [4000, 5316],
-    "a_20201" : [4000, 5205],
-    "a_20205" : [900, 1157],
-    "a_20215" : [600, 771],
-    "a_20219" : [4000, 5620],
-    "a_20221" : [4000, 5510],
-    "a_20225" : [900, 1111],
-    "a_20229" : [4000, 5916],
-    "a_20231" : [4000, 5861],
+    "a_20189": [3000, 5262],
+    "a_20191": [3000, 5189],
+    "a_20195": [300, 496],
+    "a_20199": [3000, 5316],
+    "a_20201": [3000, 5205],
+    "a_20205": [900, 1157],
+    "a_20215": [600, 771],
+    "a_20219": [3000, 5620],
+    "a_20221": [3000, 5510],
+    "a_20225": [900, 1111],
+    "a_20229": [3000, 5916],
+    "a_20231": [3000, 5861],
 }
 
-SEMESTER = "a_20195"
+SEMESTER = "a_20189"
 
 NUMBER_INSTANCES = 5
 INSTANCE_SPACE = [
     {
-        "num_students": int(np.linspace(
-            student_per_semester[SEMESTER][0], 
-            student_per_semester[SEMESTER][1], 
-            NUMBER_INSTANCES)[i]),
+        "num_students": int(
+            np.linspace(
+                student_per_semester[SEMESTER][0],
+                student_per_semester[SEMESTER][1],
+                NUMBER_INSTANCES,
+            )[i]
+        ),
         "every_n_room": 1,
         "np_seed": i,
         "room_avail_p": (np.linspace(99, 95, NUMBER_INSTANCES) / 100)[i],
         "prof_avail_p": (np.linspace(99, 95, NUMBER_INSTANCES) / 100)[i],
         "semester_date": SEMESTER,
-    } for i in range(NUMBER_INSTANCES)
+    }
+    for i in range(NUMBER_INSTANCES)
 ]
 
 
-def get_dataset(num_students, every_n_room, np_seed, room_avail_p, prof_avail_p, semester_date):
+def get_dataset(
+    num_students, every_n_room, np_seed, room_avail_p, prof_avail_p, semester_date
+):
     file = "Exam Sched Prog Datasets.xlsx"
     rooms = pd.read_excel(file, sheet_name="datasets room caps ")
-    schedule_20221 = pd.read_excel(
-        file, sheet_name="20221 Final Schedule_fromLSM")
+    schedule_20221 = pd.read_excel(file, sheet_name="20221 Final Schedule_fromLSM")
 
     hist_anonymized_enrolments = pd.read_excel(
-        file, sheet_name="hist anonymized enrolments")
+        file, sheet_name="hist anonymized enrolments"
+    )
 
-    schedule_20221['Examdate'] = schedule_20221['Examdate'].astype(str)
-    schedule_20221['Begin'] = schedule_20221['Begin'].astype(str)
-    examdate_time_list = (schedule_20221['Examdate'].dropna().astype(
-        str) + " : " + schedule_20221['Begin']).dropna().tolist()
+    schedule_20221["Examdate"] = schedule_20221["Examdate"].astype(str)
+    schedule_20221["Begin"] = schedule_20221["Begin"].astype(str)
+    examdate_time_list = (
+        (
+            schedule_20221["Examdate"].dropna().astype(str)
+            + " : "
+            + schedule_20221["Begin"]
+        )
+        .dropna()
+        .tolist()
+    )
 
     ETL = np.unique(examdate_time_list)
     examdate_time_list = [i for i in ETL if i != "NaT : nan"]
 
     # new processing
-    rooms['Room'] = rooms['Room'].astype(str)
-    rooms['Room'] = rooms['Bd'].str.cat(rooms['Room'], sep=" ")
+    rooms["Room"] = rooms["Room"].astype(str)
+    rooms["Room"] = rooms["Bd"].str.cat(rooms["Room"], sep=" ")
 
-    ava_rooms = rooms['Room'].values
-    ava_room_cap = rooms['Room Cap'].values
-    hist_anonymized_enrolments['ACAD_ACT_CD'] = hist_anonymized_enrolments['ACAD_ACT_CD'].astype(
-        str)
-    hist_anonymized_enrolments['ACAD_ACT_CD'] = hist_anonymized_enrolments['ACAD_ACT_CD'].str.cat(
-        hist_anonymized_enrolments['SECTION_CD'])
+    ava_rooms = rooms["Room"].values
+    ava_room_cap = rooms["Room Cap"].values
+    hist_anonymized_enrolments["ACAD_ACT_CD"] = hist_anonymized_enrolments[
+        "ACAD_ACT_CD"
+    ].astype(str)
+    hist_anonymized_enrolments["ACAD_ACT_CD"] = hist_anonymized_enrolments[
+        "ACAD_ACT_CD"
+    ].str.cat(hist_anonymized_enrolments["SECTION_CD"])
 
-    semeser_list = np.unique(hist_anonymized_enrolments['SESSION_CD']).tolist()
+    semeser_list = np.unique(hist_anonymized_enrolments["SESSION_CD"]).tolist()
     anan_sem = {}
     for sem in semeser_list:
-        anan_sem["a_{}".format(
-            sem)] = hist_anonymized_enrolments[hist_anonymized_enrolments['SESSION_CD'] == sem]
+        anan_sem["a_{}".format(sem)] = hist_anonymized_enrolments[
+            hist_anonymized_enrolments["SESSION_CD"] == sem
+        ]
 
     semester_df = anan_sem[semester_date]
 
@@ -98,11 +114,16 @@ def get_dataset(num_students, every_n_room, np_seed, room_avail_p, prof_avail_p,
             course_size = semester_df.groupby("ACAD_ACT_CD").size().loc[course]
             if course_size > max(ava_room_cap):
                 split = True
-                idx_students_in_course = semester_df[semester_df["ACAD_ACT_CD"] == course].index
+                idx_students_in_course = semester_df[
+                    semester_df["ACAD_ACT_CD"] == course
+                ].index
                 half_students = int(len(idx_students_in_course) / 2)
-                semester_df.loc[idx_students_in_course[:half_students],"ACAD_ACT_CD"] = course + '_1'
-                semester_df.loc[idx_students_in_course[half_students:],"ACAD_ACT_CD"] = course + '_2'
-
+                semester_df.loc[
+                    idx_students_in_course[:half_students], "ACAD_ACT_CD"
+                ] = (course + "_1")
+                semester_df.loc[
+                    idx_students_in_course[half_students:], "ACAD_ACT_CD"
+                ] = (course + "_2")
 
     def processing_fun(semester, num_student):
         name = []
@@ -120,7 +141,8 @@ def get_dataset(num_students, every_n_room, np_seed, room_avail_p, prof_avail_p,
 
         for ID in Student_ID_List:
             Student_ID_Exams.append(
-                semester["ACAD_ACT_CD"][semester.eq(ID).any(1)].values)
+                semester["ACAD_ACT_CD"][semester.eq(ID).any(1)].values
+            )
 
         for exam in Student_ID_Exams:
             exam_list.append(exam.any())
@@ -129,12 +151,16 @@ def get_dataset(num_students, every_n_room, np_seed, room_avail_p, prof_avail_p,
         new_enrol = pd.DataFrame(semester).copy()
         new_enrol["class"] = 1
 
-        temp_df = new_enrol[new_enrol["HASHED_PERSON_ID"].isin(
-            Student_ID_List)]
+        temp_df = new_enrol[new_enrol["HASHED_PERSON_ID"].isin(Student_ID_List)]
         final_temp = temp_df[temp_df["ACAD_ACT_CD"].isin(uniq_exams)]
 
         courses_enrollments_set = pd.pivot_table(
-            data=final_temp, values="class", index="ACAD_ACT_CD", columns=["HASHED_PERSON_ID"], fill_value=0)
+            data=final_temp,
+            values="class",
+            index="ACAD_ACT_CD",
+            columns=["HASHED_PERSON_ID"],
+            fill_value=0,
+        )
 
         return uniq_exams, Student_ID_List, courses_enrollments_set
 
@@ -143,7 +169,7 @@ def get_dataset(num_students, every_n_room, np_seed, room_avail_p, prof_avail_p,
     # storing
     exam_set = t_E
     student_set = t_S
-    datetime_slot_set = list(range(32)) #examdate_time_list
+    datetime_slot_set = list(range(64))  # examdate_time_list
     room_set = ava_rooms
     room_capacity_set = ava_room_cap
     courses_enrollments_set = t_C.values
@@ -155,14 +181,27 @@ def get_dataset(num_students, every_n_room, np_seed, room_avail_p, prof_avail_p,
     # make some rooms unavailable with some probability
     np.random.seed(np_seed)
     room_availability = np.random.choice(
-        2, size=(len(room_set), len(datetime_slot_set)),
-        p=[room_avail_p, 1 - room_avail_p])
+        2,
+        size=(len(room_set), len(datetime_slot_set)),
+        p=[room_avail_p, 1 - room_avail_p],
+    )
     prof_availability = np.random.choice(
-        2, size=(len(exam_set), len(datetime_slot_set)),
-        p=[prof_avail_p, 1 - prof_avail_p])
+        2,
+        size=(len(exam_set), len(datetime_slot_set)),
+        p=[prof_avail_p, 1 - prof_avail_p],
+    )
 
-    return (exam_set, student_set, datetime_slot_set, room_set, room_capacity_set,
-            courses_enrollments_set, room_availability, prof_availability, semester_date)
+    return (
+        exam_set,
+        student_set,
+        datetime_slot_set,
+        room_set,
+        room_capacity_set,
+        courses_enrollments_set,
+        room_availability,
+        prof_availability,
+        semester_date,
+    )
 
 
 def get_ET_instance(instance_num: int):
@@ -170,30 +209,49 @@ def get_ET_instance(instance_num: int):
     instance_config = INSTANCE_SPACE[instance_num]
 
     # load dataset
-    (exam_set, student_set, datetime_slot_set, room_set,
-     room_capacity_set, courses_enrollments_set,
-     room_availability, prof_availability, semester_date) = get_dataset(**instance_config)
+    (
+        exam_set,
+        student_set,
+        datetime_slot_set,
+        room_set,
+        room_capacity_set,
+        courses_enrollments_set,
+        room_availability,
+        prof_availability,
+        semester_date,
+    ) = get_dataset(**instance_config)
 
     # Create instance
-    instance = ET_Instance(exam_set, student_set, datetime_slot_set, room_set,
-                           room_capacity_set, courses_enrollments_set, 1/60, room_availability, prof_availability, semester_date)
+    instance = ET_Instance(
+        exam_set,
+        student_set,
+        datetime_slot_set,
+        room_set,
+        room_capacity_set,
+        courses_enrollments_set,
+        1 / 60,
+        room_availability,
+        prof_availability,
+        semester_date,
+    )
 
     return instance
 
 
 class ET_Instance:
-    def __init__(self,
-                 exam_set: List,
-                 student_set: List,
-                 datetime_slot_set: List,
-                 room_set: List,
-                 room_capacity_set: List,
-                 courses_enrollments_set,
-                 ratio_inv_students: float,
-                 room_availability: float,
-                 prof_availability: float,
-                 semester_date: str,
-                 ):
+    def __init__(
+        self,
+        exam_set: List,
+        student_set: List,
+        datetime_slot_set: List,
+        room_set: List,
+        room_capacity_set: List,
+        courses_enrollments_set,
+        ratio_inv_students: float,
+        room_availability: float,
+        prof_availability: float,
+        semester_date: str,
+    ):
         self.exam_set = exam_set
         self.student_set = student_set
         self.datetime_slot_set = datetime_slot_set
@@ -205,7 +263,7 @@ class ET_Instance:
         self.prof_availability = prof_availability
         self.semester_date = semester_date
 
-        print('generated a new IT instance')
+        print("generated a new IT instance")
         self.print_instance_info()
 
     def print_instance_info(self):
--- src/solver.py
+++ src/solver.py
@@ -22,69 +22,100 @@ class CplexSolver:
         mip_branching_direction = configuration_parameters["mip_branching_direction"]
         backtracking_tolerance = configuration_parameters["backtracking_tolerance"]
         mip_cliques_switch = configuration_parameters["mip_cliques_switch"]
-        coefficient_reduction_setting = configuration_parameters["coefficient_reduction_setting"]
+        coefficient_reduction_setting = configuration_parameters[
+            "coefficient_reduction_setting"
+        ]
         mip_covers_switch = configuration_parameters["mip_covers_switch"]
-        number_of_cutting_plane_passes = configuration_parameters["number_of_cutting_plane_passes"]
+        number_of_cutting_plane_passes = configuration_parameters[
+            "number_of_cutting_plane_passes"
+        ]
         cut_factor_row_multiplier_limit = configuration_parameters[
-            "cut_factor_row_multiplier_limit"]
+            "cut_factor_row_multiplier_limit"
+        ]
         mip_dive_strategy = configuration_parameters["mip_dive_strategy"]
-        dual_simplex_pricing_algorithm = configuration_parameters["dual_simplex_pricing_algorithm"]
+        dual_simplex_pricing_algorithm = configuration_parameters[
+            "dual_simplex_pricing_algorithm"
+        ]
         mip_subproblem_algorithm = configuration_parameters["mip_subproblem_algorithm"]
-        mip_node_selection_strategy = configuration_parameters["mip_node_selection_strategy"]
+        mip_node_selection_strategy = configuration_parameters[
+            "mip_node_selection_strategy"
+        ]
         mip_variable_selection_strategy = configuration_parameters[
-            "mip_variable_selection_strategy"]
+            "mip_variable_selection_strategy"
+        ]
 
-        self.optimizer = Model(name='solver')
+        self.optimizer = Model(name="solver")
         self.optimizer.parameters.timelimit = timelimit
         self.optimizer.parameters.lpmethod = lpmethod
         self.optimizer.parameters.mip.strategy.bbinterval = mip_s_bbinterval
         self.optimizer.parameters.mip.strategy.branch = mip_branching_direction
         self.optimizer.parameters.mip.strategy.backtrack = backtracking_tolerance
         self.optimizer.parameters.mip.cuts.cliques = mip_cliques_switch
-        self.optimizer.parameters.preprocessing.coeffreduce = coefficient_reduction_setting
+        self.optimizer.parameters.preprocessing.coeffreduce = (
+            coefficient_reduction_setting
+        )
         self.optimizer.parameters.mip.cuts.covers = mip_covers_switch
         self.optimizer.parameters.mip.limits.cutpasses = number_of_cutting_plane_passes
-        self.optimizer.parameters.mip.limits.cutsfactor = cut_factor_row_multiplier_limit
+        self.optimizer.parameters.mip.limits.cutsfactor = (
+            cut_factor_row_multiplier_limit
+        )
         self.optimizer.parameters.mip.strategy.dive = mip_dive_strategy
         self.optimizer.parameters.simplex.dgradient = dual_simplex_pricing_algorithm
         self.optimizer.parameters.mip.strategy.subalgorithm = mip_subproblem_algorithm
         self.optimizer.parameters.mip.strategy.nodeselect = mip_node_selection_strategy
-        self.optimizer.parameters.mip.strategy.variableselect = mip_variable_selection_strategy
+        self.optimizer.parameters.mip.strategy.variableselect = (
+            mip_variable_selection_strategy
+        )
 
         return
 
     def add_variables(self, E, T, R, S):
-        """
-        """
-        x = self.optimizer.binary_var_matrix(len(E), len(
-            T), name="X_e,t")  # whether we use timeslot t for exam e
-        y = self.optimizer.binary_var_matrix(len(E), len(
-            R), name="Y_e,r")  # whether we use room r for exam e
-        x_etr = self.optimizer.binary_var_cube(
-            len(E), len(T), len(R), name='xetr')
+        """ """
+        x = self.optimizer.binary_var_matrix(
+            len(E), len(T), name="X_e,t"
+        )  # whether we use timeslot t for exam e
+        y = self.optimizer.binary_var_matrix(
+            len(E), len(R), name="Y_e,r"
+        )  # whether we use room r for exam e
+        x_etr = self.optimizer.binary_var_cube(len(E), len(T), len(R), name="xetr")
 
-        x_st = self.optimizer.binary_var_matrix(len(S), len(
-            T), name="x_st")  # whether student s in sitting in an exam at time T
+        x_st = self.optimizer.binary_var_matrix(
+            len(S), len(T), name="x_st"
+        )  # whether student s in sitting in an exam at time T
 
         return x, y, x_etr, x_st
 
     def add_constraints(self, E, S, T, R, Cp, He_s, sumHe_s, x, y, x_etr, x_st):
         print("Loading constraints")
         self.optimizer.add_constraints(
-            (sum(x[e, t] for t in range(len(T))) == 1
-                for e in range(len(E))), names='c1')
+            (sum(x[e, t] for t in range(len(T))) == 1 for e in range(len(E))),
+            names="c1",
+        )
         self.optimizer.add_constraints(
-            (sum(y[e, r] for r in range(len(R))) >= 1
-                for e in range(len(E))), names='c2')
+            (sum(y[e, r] for r in range(len(R))) >= 1 for e in range(len(E))),
+            names="c2",
+        )
         self.optimizer.add_constraints(
-            (sum(x_etr[e, t, r] for r in range(len(R))) == x[e, t]
-                for e in tqdm(range(len(E))) for t in range(len(T))))
+            (
+                sum(x_etr[e, t, r] for r in range(len(R))) == x[e, t]
+                for e in tqdm(range(len(E)))
+                for t in range(len(T))
+            )
+        )
         self.optimizer.add_constraints(
-            (sum(x_etr[e, t, r] for t in range(len(T))) == y[e, r]
-                for e in tqdm(range(len(E))) for r in range(len(R))))
+            (
+                sum(x_etr[e, t, r] for t in range(len(T))) == y[e, r]
+                for e in tqdm(range(len(E)))
+                for r in range(len(R))
+            )
+        )
         self.optimizer.add_constraints(
-            (sum(x_etr[e, t, r] * sumHe_s[e] for e in range(len(E))) <= Cp[r]
-                for r in tqdm(range(len(R))) for t in range(len(T))))
+            (
+                sum(x_etr[e, t, r] * sumHe_s[e] for e in range(len(E))) <= Cp[r]
+                for r in tqdm(range(len(R)))
+                for t in range(len(T))
+            )
+        )
         # c6
         for s in tqdm(range(len(S))):
             for t in range(len(T)):
@@ -100,7 +131,7 @@ class CplexSolver:
         #     for t in range(len(T)):
         #         for s in range(len(S)):
         #             self.optimizer.add_constraint(x_st[s,t] == (x[e, t] * He_s[e, s]))
-        
+
         # C8 only one exam per day for each student
         for s in range(len(S)):
             k = 0
@@ -111,29 +142,32 @@ class CplexSolver:
                         sum_xt += x_st[s, k + j]
                     self.optimizer.add_constraint(sum_xt <= 1)
                 else:
-                    self.optimizer.add_constraint((x_st[s, k] + x_st[s, k+1] + x_st[s, k+2]) <= 1)
+                    self.optimizer.add_constraint(
+                        (x_st[s, k] + x_st[s, k + 1] + x_st[s, k + 2]) <= 1
+                    )
                 k += 3
 
-
-    def add_situational_constraints(self, E, R, x, x_etr, room_availability, prof_availability):
+    def add_situational_constraints(
+        self, E, R, x, x_etr, room_availability, prof_availability
+    ):
         for idx in np.argwhere(room_availability == 1):
             r = idx[0]
             t = idx[1]
 
-            self.optimizer.add_constraints(
-                (x_etr[e, t, r] == 0) for e in range(len(E)))
+            self.optimizer.add_constraints((x_etr[e, t, r] == 0) for e in range(len(E)))
 
         for idx in np.argwhere(prof_availability == 1):
             e = idx[0]
             t = idx[1]
 
             self.optimizer.add_constraint(x[e, t] == 0)
-            self.optimizer.add_constraints(
-                (x_etr[e, t, r] == 0) for r in range(len(R)))
+            self.optimizer.add_constraints((x_etr[e, t, r] == 0) for r in range(len(R)))
 
     def add_objective_function(self, y, E, R, sumHe_s, ratio_of_Inv):
-        up = (sum(1 * sumHe_s[e] * ratio_of_Inv for e in range(len(E)))
-              for r in range(len(R)))
+        up = (
+            sum(1 * sumHe_s[e] * ratio_of_Inv for e in range(len(E)))
+            for r in range(len(R))
+        )
         upper_bound = 0
         for i in up:
             upper_bound += np.ceil(i)
@@ -143,17 +177,18 @@ class CplexSolver:
 
         for r in range(len(R)):
             ceil_obj.append(self.optimizer.integer_var(lb=0, ub=upper_bound))
-            sum_sum.append(sum(y[e, r] * sumHe_s[e] *
-                               ratio_of_Inv for e in range(len(E))))
+            sum_sum.append(
+                sum(y[e, r] * sumHe_s[e] * ratio_of_Inv for e in range(len(E)))
+            )
             self.optimizer.add_constraint(ceil_obj[r] >= sum_sum[r])
 
         obj_fun = sum(ceil_obj[r] for r in range(len(R)))
 
         # Optimizer Info
-        self.optimizer.set_objective('min', obj_fun)
+        self.optimizer.set_objective("min", obj_fun)
         self.optimizer.print_information()
 
-    def solve(self, problem_instance, save_filepath: str = '', verbose=False):
+    def solve(self, problem_instance, save_filepath: str = "", verbose=False):
         """
         Solves a problem instance
         """
@@ -177,7 +212,8 @@ class CplexSolver:
 
         # Optional Constraints
         self.add_situational_constraints(
-            E, R, x, x_etr, room_availability, prof_availability)
+            E, R, x, x_etr, room_availability, prof_availability
+        )
 
         # Objective Function
         self.add_objective_function(y, E, R, sumHe_s, ratio_of_Inv)
@@ -188,16 +224,16 @@ class CplexSolver:
             log_output = True
         else:
             log_output = False
-        sol = self.optimizer.solve(
-            log_output=log_output, clean_before_solve=True)
+        sol = self.optimizer.solve(log_output=log_output, clean_before_solve=True)
 
         # process the solution
         if sol:
             print("Found a solution \n")
             schedule, df_x, df_y = self.process_solution(sol, x, y, E, T, R)
             enrolment_df = create_enrolment_df(He_s, S, E)
-            df_schedule = (schedule.merge(enrolment_df, on='EXAM', how='left')).drop(
-                ["exam_x", "value_x", "exam_y", "room", "value_y"], axis=1)
+            df_schedule = (schedule.merge(enrolment_df, on="EXAM", how="left")).drop(
+                ["exam_x", "value_x", "exam_y", "room", "value_y"], axis=1
+            )
             solve_time = self.optimizer.solve_details.time
             objective_value = self.optimizer.objective_value
 
@@ -205,7 +241,7 @@ class CplexSolver:
             print("Could not find a solution")
             solve_time = 1e6
             objective_value = 1e6
-            df_schedule = pd.DataFrame({'F': ["Failed :("]})
+            df_schedule = pd.DataFrame({"F": ["Failed :("]})
 
         # write to file
         if len(save_filepath):
@@ -216,20 +252,19 @@ class CplexSolver:
                 os.makedirs(path)
 
             for v in self.optimizer.iter_binary_vars():
-                with open(save_filepath, 'a') as f:
+                with open(save_filepath, "a") as f:
                     if sol:
                         f.write(f"{v} = {v.solution_value} \n")
                     else:
                         f.write(f"{v} = {np.nan} \n")
 
-            with open(save_filepath, 'a') as f:
+            with open(save_filepath, "a") as f:
                 f.write(f"rt = {solve_time} \n")
                 f.write(f"obj = {objective_value} \n")
 
         self.optimizer.clear()
 
         return solve_time, objective_value, df_schedule
-
 
     def process_solution(self, sol, x, y, E, T, R):
         """
@@ -253,9 +288,9 @@ class CplexSolver:
         """
         # extract solutions as df
         df_x = sol.get_value_df(x).rename(
-            columns={'key_1': 'exam', 'key_2': 'timeslot'})
-        df_y = sol.get_value_df(y).rename(
-            columns={'key_1': 'exam', 'key_2': 'room'})
+            columns={"key_1": "exam", "key_2": "timeslot"}
+        )
+        df_y = sol.get_value_df(y).rename(columns={"key_1": "exam", "key_2": "room"})
 
         # Add rows with the names of courses and timelots
         exam_col = [E[i] for i in range(len(E)) for j in range(len(T))]
@@ -271,11 +306,12 @@ class CplexSolver:
 
         # Produce the final schedule
         final_schedule = df_x[df_x["value"] == 1].merge(
-            df_y[df_y["value"] == 1], on='EXAM', how='left')
-        final_schedule = final_schedule.sort_values(
-            by=["timeslot"], ascending=True)
+            df_y[df_y["value"] == 1], on="EXAM", how="left"
+        )
+        final_schedule = final_schedule.sort_values(by=["timeslot"], ascending=True)
 
         return final_schedule, df_x, df_y
+
 
 def create_enrolment_df(He_s: np.array, S, E) -> pd.DataFrame:
     """
@@ -289,8 +325,8 @@ def create_enrolment_df(He_s: np.array, S, E) -> pd.DataFrame:
                 students_in_exam_e.append(S[i])
         exam_student_pairs.append(students_in_exam_e)
 
-    enrolment_df = pd.DataFrame(columns=['EXAM', 'student'])
-    enrolment_df['EXAM'] = E
-    enrolment_df['student'] = exam_student_pairs
+    enrolment_df = pd.DataFrame(columns=["EXAM", "student"])
+    enrolment_df["EXAM"] = E
+    enrolment_df["student"] = exam_student_pairs
 
     return enrolment_df
